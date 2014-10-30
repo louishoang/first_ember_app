f1bc06dded2befb23bdb913f7675467c254ba4d5
louis hoang <bawsing@gmail.com>
2014-10-30 15:22:21 -0400

    pull info from API github

--- js/app.js
+++ js/app.js
@@ -1,17 +1,25 @@
-App = Ember.Application.create();
+App = Ember.Application.create(); // make a new app call App
 
 App.Router.map(function() {
+
   // put your routes here
   // routes index is not neccessary
+  this.resource("members", function(){
+    this.resource("show");
+  });
 });
-
-
-
 // similar to control in rails
+
 App.IndexRoute = Ember.Route.extend({
   //render template
   model: function() {
-    return ['louis', 'yellow', 'blue'];
-    you sucks;
+    // var organization = {
+    //   name: "Launch Academy",
+    //   location: "Boston, MA",
+    //   login: "launchacademy",
+    //   email: "hello@launchacademy.com"
+    // };
+    var organization = Ember.$.getJSON("https://api.github.com/orgs/LaunchAcademy")
+    return organization;
   }
 });
